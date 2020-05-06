c801fda3ef0624187b9806ff9468e7a982b231dd
Jacob F. Grant <git@jacobfgrant.com>
2020-05-05 22:00:29 -0700

    Create script to stow dotfiles
    Creates a script that uses the tool stow to symlink dotfiles in this
    directory to the user's home directory.

--- stow.sh
+++ stow.sh
@@ -1,9 +1,26 @@
 #/bin/sh
+#
+# Script to use GNU stow to symlink dotifles.
+#
+# Run this script to symlink the contents of the directories in the same directory as this
+# script to the user's home directory. 
+#
+#
+# Author:  Jacob F. Grant
+# Created: 05/05/2020
+#
+
 
 DOTFILES_DIR=$(dirname "$0")
+STOW=$(which stow)
 
-echo "$DOTFILES_DIR"
-
-cd "$DOTFILES_DIR"
-
-find . -maxdepth 1 -type d -not -name '.git' | sed -n 's/\.\/\(.*\)/\1/p' |  xargs -n 1 stow -R
+# Check that stow is installed
+if [ -z "$STOW" ]
+then
+    echo "'stow' is not installed"
+    exit 1
+else
+    # Stow dotfiles
+    cd "$DOTFILES_DIR"
+    find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' | xargs -n 1 basename | xargs -n 1 `which stow` -t "$HOME" -R
+fi
