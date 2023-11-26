fd8f0220165740bc4efe85ef2caccc4af4da1406
Jacob F. Grant <git@jacobfgrant.com>
2023-11-26 14:13:12 -0800

    Improve stow.sh script
    Updates the stow.sh script to improve clarity, add error handling, and
    make it more portable by checking for stow installed using Homebrew.

--- stow.sh
+++ stow.sh
@@ -1,26 +1,36 @@
-#/bin/sh
+#!/usr/bin/env sh
 #
-# Script to use GNU stow to symlink dotifles.
+# Script to use GNU stow to symlink dotfiles.
 #
-# Run this script to symlink the contents of the directories in the same directory as this
-# script to the user's home directory. 
+# This script creates symlinks in the user's home directory for the contents of each directory 
+# located in the same directory as this script. It utilizes GNU Stow for symlink management.
 #
 #
 # Author:  Jacob F. Grant
 # Created: 05/05/2020
 #
 
-
 DOTFILES_DIR=$(dirname "$0")
-STOW=$(which stow)
 
-# Check that stow is installed
+# Try to locate stow, check /opt/homebrew/bin/stow if not found
+STOW=$(which stow 2>/dev/null)
 if [ -z "$STOW" ]
 then
-    echo "'stow' is not installed"
-    exit 1
-else
-    # Stow dotfiles
-    cd "$DOTFILES_DIR"
-    find . -mindepth 1 -maxdepth 1 -type d -not -name '.git' | xargs -n 1 basename | xargs -n 1 `which stow` -t "$HOME" -R
+    if [ -x "/opt/homebrew/bin/stow" ]
+    then
+        STOW="/opt/homebrew/bin/stow"
+    else
+        echo "'stow' is not installed"
+        exit 1
+    fi
 fi
+
+# Stow dotfiles
+cd "$DOTFILES_DIR" || exit
+for dir in */
+do
+    if [ "$dir" != ".git/" ] && [ "$(ls -A "$dir")" ]
+    then
+        $STOW -t "$HOME" -R "${dir%/}"
+    fi
+done
